6560230c6cc8a2164920aed17a290636b9f36881
kaihsinwu <kaihsinwu@gmail.com>
2018-11-16 18:49:39 +0800

    fix odd increment bug

--- Code/baseline.cu
+++ Code/baseline.cu
@@ -6,7 +6,7 @@ __device__ uint32_t Generate(volatile uint64_t &state, uint64_t inc){
     uint32_t xorshifted = ((state >> 18u)^state) >> 27u;
     uint32_t rot = state >> 59u;
     // Update state
-    state = (state * 6364136223846793005ULL + inc);
+    state = (state * 6364136223846793005ULL + 2*inc+1);
     return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
 
